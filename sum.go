0f7718e1bbe4892a2e7d2ab6876c3bfd345a1e63
Isaac Silva <yzaak.silva@gmail.com>
2022-06-12 17:25:09 -0300

    refactor SumAllTails function and its test

--- sum.go
+++ sum.go
@@ -29,9 +29,13 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _,numbers := range numbersToSum{
 		//remove head by slicing from the second to the last element
-		tail := numbers[1:]
-		sums = append(sums,Sum(tail))
-
+		if len(numbers) == 0{
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums,Sum(tail))
+		}
 	}
+	
 	return sums
 }
