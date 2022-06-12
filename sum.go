e62931641243fbff2d3264cf4fb6a7b7ba032ae4
Isaac Silva <yzaak.silva@gmail.com>
2022-06-12 17:00:48 -0300

    refactor SumAll

--- sum.go
+++ sum.go
@@ -12,14 +12,12 @@ func Sum(numbers[] int) (sum int){
 //sum the elements of each inner slices
 //and return a new slice of all sums
 func SumAll(numbersToSum ...[]int) []int {
-	length := len(numbersToSum)
 	//create a new empty slice to record the sum
-	//with the length of total slices passed
-	sums := make([]int, length)
+	var sums []int
 
 	//sum each slice and record the total in slice sum
-	for i, numbers := range numbersToSum{
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum{
+		sums = append(sums,Sum(numbers))
 	}
 	
 	return sums
